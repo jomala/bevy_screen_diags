7d78970dcde7376b28d8fcf05278c5ce92ac83a7
Alex Saveau <saveau.alexandre@gmail.com>
2022-01-25 20:03:37 -0800

    Use 2 decimals of precision
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -79,7 +79,7 @@ fn format_fps(s: &mut String, fps: f64) {
     s.clear();
     // SAFETY: Writing to a String never fails
     unsafe {
-        write!(s, "{:4.0}", fps).unwrap_unchecked();
+        write!(s, "{:.2}", fps).unwrap_unchecked();
     }
 }
 
